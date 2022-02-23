a57cd202f5ec196a4ed29a60505309eb60545bf2
moruno21 <amoruno21@gmail.com>
2022-02-23 13:49:33 +0100

    first two exercises

---
+++ Tercero/issbc/p1/e1.py
@@ -0,0 +1,20 @@
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+
+def main():
+
+    app = QApplication(sys.argv)
+
+    w = QWidget()
+    w.resize(250, 150)
+    w.move(300, 300)
+    w.setWindowTitle('Simple')
+    w.show()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
---
+++ Tercero/issbc/p1/e2.py
@@ -0,0 +1,31 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+
+    def initUI(self):
+
+        self.setGeometry(300, 300, 300, 220)
+        self.setWindowTitle('Icon')
+        self.setWindowIcon(QIcon('web.png'))
+
+        self.show()
+
+
+def main():
+
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
