6de685788247f52d463fcf5fca1b5debd1f5ff4f
moruno21 <amoruno21@gmail.com>
2022-03-22 11:14:02 +0100

    p2 finished

--- Tercero/issbc/p2/app-editor-textos.py
+++ Tercero/issbc/p2/app-editor-textos.py
@@ -1,64 +1,90 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QIcon
 import sys
-from PyQt5.QtWidgets import QMainWindow, QAction, QTextEdit, QFileDialog, QApplication
-from PyQt5 import QtGui, QtCore
+from pathlib import Path
 
 
-class Main(QMainWindow):
-
+class Editor_texto(QMainWindow):
     def __init__(self):
-        QMainWindow.__init__(self)
+        super().__init__()
         self.initUI()
 
     def initUI(self):
+        self.path = None
+        self.txt = QTextEdit()
+        self.setCentralWidget(self.txt)
+        self.statusBar()
 
-        newAction = QAction('Nuevo', self)
-        newAction.setShortcut('Ctrl+N')
-        newAction.triggered.connect(self.newFile)
+        newFile = QAction("Nuevo", self)
+        newFile.setShortcut("Ctrl+N")
+        newFile.setStatusTip("Nuevo archivo")
+        newFile.triggered.connect(self.newFile)
 
-        saveAction = QAction('Guardar', self)
-        saveAction.setShortcut('Ctrl+S')
-        saveAction.triggered.connect(self.saveFile)
+        openFile = QAction("Abrir", self)
+        openFile.setShortcut("Ctrl+O")
+        openFile.setStatusTip("Abrir archivo")
+        openFile.triggered.connect(self.openFile)
 
-        openAction = QAction('Abrir', self)
-        openAction.setShortcut('Ctrl+O')
-        openAction.triggered.connect(self.openFile)
+        saveFile = QAction("Guardar", self)
+        saveFile.setShortcut("Ctrl+S")
+        saveFile.setStatusTip("Guardar Archivo")
+        saveFile.triggered.connect(self.saveFile)
+
+        saveAsFile = QAction("Guardar como", self)
+        saveAsFile.setShortcut("F12")
+        saveAsFile.setStatusTip("Guardar como")
+        saveAsFile.triggered.connect(self.saveAsFile)
 
         menubar = self.menuBar()
-        fileMenu = menubar.addMenu('Archivo')
-        fileMenu.addAction(newAction)
-        fileMenu.addAction(saveAction)
-        fileMenu.addAction(openAction)
+        fileMenu = menubar.addMenu("&Archivo")
+        fileMenu.addAction(newFile)
+        fileMenu.addAction(openFile)
+        fileMenu.addAction(saveFile)
+        fileMenu.addAction(saveAsFile)
 
-        self.txt = QTextEdit(self)
-        self.setCentralWidget(self.txt)
-
-        self.setGeometry(300, 300, 300, 300)
-        self.setWindowTitle("Editor de textos")
+        self.setGeometry(300, 300, 500, 500)
+        self.setWindowTitle("Editor de texto")
         self.show()
 
     def newFile(self):
         self.txt.clear()
 
-    def saveFile(self):
-        filename = QFileDialog.getSaveFileName(self, 'Guardar Archivo')
-        f = open(filename[0], 'w')
-        filedata = self.txt.toPlainText()
-        f.write(filedata)
-        f.close()
-
     def openFile(self):
-        filename = QFileDialog.getOpenFileName(self, 'Abrir Archivo')
-        f = open(filename[0], 'r')
-        filedata = f.read()
-        self.txt.setText(filedata)
-        f.close()
+        home_dir = str(Path.home())
+        fname = QFileDialog.getOpenFileName(self, "Abrir archivo", home_dir)
+        if fname[0]:
+            f = open(fname[0], "r")
+            with f:
+                data = f.read()
+                self.txt.setText(data)
+                self.path = fname
+                self.setWindowTitle(fname[0])
+
+    def saveFile(self):
+        if self.path is None:
+            return self.saveAsFile()
+        self.save_path()
+
+    def saveAsFile(self):
+        fname = QFileDialog.getSaveFileName(self, "Guardar archivo")
+        if fname[0]:
+            f = open(fname[0], "w")
+            with f:
+                data = self.txt.toPlainText()
+                f.write(data)
+                f.close()
+                self.path = fname
+                self.setWindowTitle(fname[0])
+
+    def save_path(self):
+        text = self.txt.toPlainText()
+        with open(self.path[0], "w") as f:
+            f.write(text)
 
 
 def main():
     app = QApplication(sys.argv)
-    main = Main()
-    main.show()
-
+    ex = Editor_texto()
     sys.exit(app.exec_())
 
 
