b3469684502eac362615d4db94b2692a51c066ab
moruno21 <amoruno21@gmail.com>
2022-03-23 21:53:28 +0100

    files modified

--- Tercero/issbc/examPractice/mvcEditor/modelEditor.py
+++ Tercero/issbc/examPractice/mvcEditor/modelEditor.py
@@ -1,36 +1,33 @@
 # -*- coding: utf-8 -*-
 
 
-from PyQt5.QtWidgets import QFileDialog, QTreeView, QListView, QFileSystemModel
-from PyQt5.QtCore import QDir
+from PyQt5.QtWidgets import (QFileDialog, QTreeView, QListView, QFileSystemModel)
+from PyQt5.QtCore import (QDir)
 from pathlib import Path
 
 
 def openFileDialog(self):
-    home_dir = str(Path.home())
-    fname = QFileDialog.getOpenFileName(self, "Open file", home_dir)
-    self.setWindowTitle(fname[0])
-    if fname[0]:
-        f = open(fname[0], "r")
-        with f:
-            data = f.read()
-            self.textArea.setText(data)
-            self.filePath = fname
+        home_dir = str(Path.home())
+        fname = QFileDialog.getOpenFileName(self, 'Open file', home_dir)
+        self.setWindowTitle(fname[0])
+        if fname[0]:
+            f = open(fname[0], 'r')
+            with f:
+                data = f.read()
+                self.textArea.setText(data)
+                self.filePath = fname
 
 
 def onFolderClicked(self, index):
-    path = self.dirModel.fileInfo(index).absoluteFilePath()
-    self.listview.setRootIndex(self.fileModel.setRootPath(path))
+        path = self.dirModel.fileInfo(index).absoluteFilePath()
+        self.listview.setRootIndex(self.fileModel.setRootPath(path))
 
 
 def fileSaveAs(self):
-    fname = QFileDialog.getSaveFileName(
-        self,
-        "Save file",
-    )
+    fname = QFileDialog.getSaveFileName(self, 'Save file',)
     self.setWindowTitle(fname[0])
     if fname[0]:
-        f = open(fname[0], "w")
+        f = open(fname[0], 'w')
         with f:
             data = self.textArea.toPlainText()
             f.write(data)
@@ -46,7 +43,7 @@ def fileSave(self):
 
 def fileSaveToPath(self):
     text = self.textArea.toPlainText()
-    with open(self.filePath[0], "w") as f:
+    with open(self.filePath[0], 'w') as f:
         f.write(text)
 
 
@@ -58,7 +55,7 @@ def treeAndListView(self):
     self.dirModel.setRootPath(QDir.rootPath())
     self.dirModel.setFilter(QDir.NoDotAndDotDot | QDir.AllDirs)
     self.fileModel = QFileSystemModel()
-    self.fileModel.setFilter(QDir.NoDotAndDotDot | QDir.Files)
+    self.fileModel.setFilter(QDir.NoDotAndDotDot |  QDir.Files)
     self.treeview.setModel(self.dirModel)
     self.listview.setModel(self.fileModel)
     self.treeview.setRootIndex(self.dirModel.index(path))
--- Tercero/issbc/examPractice/mvcEditor/vistaEditor.py
+++ Tercero/issbc/examPractice/mvcEditor/vistaEditor.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
 
 import sys
-from PyQt5.QtWidgets import QWidget, QPushButton, QApplication, QLabel, QGridLayout, QTextEdit
+from PyQt5.QtWidgets import (QWidget, QPushButton, QApplication, QLabel, QGridLayout, QTextEdit)
 
 import controllerEditor as controller
 
 
 class Editor(QWidget):
+
     def __init__(self):
         super().__init__()
 
@@ -19,15 +20,15 @@ class Editor(QWidget):
         grid = QGridLayout()
         grid.setSpacing(10)
 
-        # folder stuff
+        #folder stuff
         controller.treeAndListView(self)
 
         # texto 'carpeta' e input para especificar la carpeta
-        folderText = QLabel("Carpeta")
-
+        folderText = QLabel('Carpeta')
+        
         # area de texto
         self.textArea = QTextEdit()
-
+        
         # set de botones laterales
         folderButton = QPushButton("Carpeta")
         openFileButton = QPushButton("Abrir archivo")
@@ -36,23 +37,25 @@ class Editor(QWidget):
         exitButton = QPushButton("Salir")
 
         # -- addWidgets to grid --
-        # tree and list view
-        # grid.addWidget(self.treeview, 1, 0)
+        #tree and list view
+        grid.addWidget(self.treeview, 1, 0)
         grid.addWidget(self.listview, 2, 0)
-        # folder text and area text
+        #folder text and area text
         grid.addWidget(folderText, 0, 0)
         grid.addWidget(self.textArea, 1, 1, 5, 1)
-        # buttons
+        #buttons
         grid.addWidget(folderButton, 0, 2)
         grid.addWidget(openFileButton, 1, 2)
         grid.addWidget(saveButton, 2, 2)
         grid.addWidget(saveAsButton, 3, 2)
         grid.addWidget(exitButton, 5, 2)
 
+
         self.setLayout(grid)
         self.setGeometry(300, 300, 300, 150)
-        self.setWindowTitle("Editor de textos")
+        self.setWindowTitle('Editor de textos')
         self.show()
+
 
         # configuración de los botones
         exitButton.clicked.connect(QApplication.instance().quit)
@@ -60,21 +63,27 @@ class Editor(QWidget):
         saveAsButton.clicked.connect(self.fileSaveAs)
         saveButton.clicked.connect(self.fileSave)
 
+    
+
     def openFileDialog(self):
         controller.openFileDialog(self)
+
 
     def onFolderClicked(self, index):
         controller.onFolderClicked(self, index)
 
+
     def fileSaveAs(self):
         controller.fileSaveAs(self)
+
 
     def fileSave(self):
         controller.fileSave(self)
 
+
     def fileSaveToPath(self):
         controller.fileSaveToPath(self)
-
+ 
 
 def main():
     app = QApplication(sys.argv)
@@ -82,5 +91,5 @@ def main():
     sys.exit(app.exec_())
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
